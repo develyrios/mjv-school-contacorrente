8beb79f25e6eaf2c6dbce434b3f83887a7f5fa05
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-10-07 20:19:33 -0300

    Principal

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -2,7 +2,6 @@ package main.java;
 
 public class Principal {
     public static void main(String[] args) {
-        ContaCorrente contaCorrente = new ContaCorrente();
-        contaCorrente.run();
+        // To do...
     }
 }
